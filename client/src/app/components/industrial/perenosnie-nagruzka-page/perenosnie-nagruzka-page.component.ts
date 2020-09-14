e031d960fdc2c13331863167c1ea7817019ee675
DEXTERMD <dextermdpay@gmail.com>
2020-09-14 13:04:10 +0300

    fix new page 6

--- client/src/app/components/industrial/perenosnie-nagruzka-page/perenosnie-nagruzka-page.component.ts
+++ client/src/app/components/industrial/perenosnie-nagruzka-page/perenosnie-nagruzka-page.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerenosnieNagruzkaPageComponent implements OnInit {
 
+  elements: any = [
+    {first: 'Наибольший предел взвешивания, т:', last: '15'},
+    {first: 'Наименьший предел взвешивания, кг:', last: '100'},
+    {first: 'Цена поверочного деления (е), кг:', last: '5'},
+    {first: 'Дискретность отсчета (d), кг:', last: '5'},
+    {first: 'Количество платформ, шт:', last: '2'},
+    {first: 'Степень защиты от пыли и воды (платформа):', last: 'IP 66'},
+    {first: 'Степень защиты от пыли и воды (индикатор MERAV2000):', last: 'IP 54'},
+    {first: 'Подключение к компьютеру', last: 'RS232'},
+    {first: 'Рабочий температурный диапазон (индикатор), С:', last: 'От -5 До +35'},
+    {first: 'Напряжение питания от сети:', last: '220В(+10%;-15%); 50 Гц +/-2%'},
+    {first: 'Класс точности весов:', last: 'IB'},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
