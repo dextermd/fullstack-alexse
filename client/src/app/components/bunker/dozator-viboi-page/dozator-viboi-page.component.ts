06e0d9d1f821b2d120ce388e11b393c4a77ec40f
DEXTERMD <dextermdpay@gmail.com>
2020-10-12 20:24:11 +0300

    translate

--- client/src/app/components/bunker/dozator-viboi-page/dozator-viboi-page.component.ts
+++ client/src/app/components/bunker/dozator-viboi-page/dozator-viboi-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dozator-viboi-page',
@@ -8,27 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class DozatorViboiPageComponent implements OnInit {
 
 
-  elements: any = [
-    {first: 'Производительность по зерну*, т/ч:', last: '4 / 5'},
-    {first: 'Вес дозы, кг:', last: '20-50 / 20-50'},
-    {first: 'Цена поверочного деления (е), г:', last: '0,05 / 0,05'},
-    {first: 'Дискретность отсчета (d), г:', last: '0,05 / 0,05'},
-    {first: 'Расход сжатого воздуха не более, нМ3/ч:', last: '- / 2,3'},
-    {first: 'Рабочее давление в системе, атм:', last: '- / 4-6'},
-    {first: 'Потребляемая мощность, не более, Вт:', last: '45'},
-    {first: 'Питание от сети:', last: '220В(+10%;-15%); 50 Гц +/-2%'},
-    {first: 'Рабочий температурный диапазон, (контроллер MERAV3001), С:', last: 'От 0 До +40'},
-    {first: 'Рабочий температурный диапазон, (бункер), С:', last: 'От -5 До +35'},
-    {first: 'Количество датчиков, шт:', last: '1'},
-    {first: 'Класс точности по ГОСТ 10223-97:', last: '1 (0,5%)'},
-    {first: 'Габаритные размеры, мм:', last: '735 Х 1050 Х 2475 / 850 Х 850 Х 2155'},
-    {first: 'Защита от пыли и влаги (контроллер MERAV3001):', last: 'IP54'},
-    {first: 'Защита от пыли и влаги (датчики):', last: 'IP68'},
-  ];
-
-
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- client/src/app/components/vagons/vagon-page/vagon-page.component.ts
+++ client/src/app/components/vagons/vagon-page/vagon-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-vagon-page',
@@ -8,23 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class VagonPageComponent implements OnInit {
 
 
-  elements: any = [
-    {first: 'Наибольший предел взвешивания, т:', last: '120'},
-    {first: 'Наименьший предел взвешивания, кг:', last: '1000'},
-    {first: 'Цена поверочного деления (е), кг:', last: '50'},
-    {first: 'Дискретность отсчета (d), кг:', last: '50'},
-    {first: 'Количество платформ, шт:', last: '2 (3)'},
-    {first: 'Длина грузоприемных платформ, м:', last: '5.5 (4)'},
-    {first: 'Количество датчиков, шт:', last: '8 (12)'},
-    {first: 'Степень защиты от пыли и воды (платформа):', last: 'IP 68'},
-    {first: 'Степень защиты от пыли и воды (индикатор MERAV2000):', last: 'IP 67'},
-    {first: 'Рабочий температурный диапазон (платформа), С:', last: 'От -30 До +45'},
-    {first: 'Рабочий температурный диапазон (индикатор), С:', last: 'От 0 До +40'},
-    {first: 'Напряжение питания от сети:', last: '220В(+10%;-15%); 50 Гц +/-2%'},
-    {first: 'Класс точности весов:', last: 'Средний (III)'},
-  ];
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
