5c7adb1c8025dbc6cc90a6d159bfb3508cbb9d6b
DEXTERMD <dextermdpay@gmail.com>
2020-10-22 13:15:04 +0300

    Google

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from './shared/auth.service';
 import { Title, Meta } from '@angular/platform-browser';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
+
+declare var gtag;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent implements OnInit {
   title = 'alexse';
-
   constructor(
     private auth: AuthService,
-    // private titleService: Title,
-    // private meta: Meta,
+    router: Router
   ) {
+    const navEndEvents = router.events.pipe(
+      filter(
+        event => event instanceof NavigationEnd
+      )
+    );
 
+    navEndEvents.subscribe(
+      (event: NavigationEnd) => {
+        gtag('config', 'G-D2DN67SM8M', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    );
   }
 
   ngOnInit(): void {
