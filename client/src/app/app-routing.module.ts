843fa8a8261315199ee4505d2450a4a177a72624
DEXTERMD <dextermdpay@gmail.com>
2020-10-23 10:55:22 +0300

    Preload

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -532,7 +532,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules,
+    // preloadingStrategy: PreloadAllModules,
     enableTracing: false,
     scrollPositionRestoration: 'top',
     initialNavigation: 'enabled'
