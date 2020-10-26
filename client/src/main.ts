f3bbd676dcd2eca1f668d17759dd81cb740b3422
DEXTERMD <dextermdpay@gmail.com>
2020-10-26 17:15:01 +0200

    prod mod

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 
-if (environment.production) {
-  enableProdMode();
-}
+// if (environment.production) {
+enableProdMode();
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule)
