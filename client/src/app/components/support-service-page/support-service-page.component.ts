2138198f9c4114c09361a2beafd6c4d696899f79
DEXTERMD <dextermdpay@gmail.com>
2020-10-26 14:12:07 +0200

    Support

--- client/src/app/components/support-service-page/support-service-page.component.ts
+++ client/src/app/components/support-service-page/support-service-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-support-service-page',
@@ -8,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupportServicePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public translate: TranslateService,
+    private titleService: Title,
+    private meta: Meta,
+  ) {
+    this.translate.get('Support.Title').subscribe(res => {
+      this.titleService.setTitle(res);
+    });
+    this.translate.get('Support.Desc').subscribe(res => {
+      this.meta.addTag({name: 'description', content: res});
+    });
+  }
 
   ngOnInit(): void {
   }
