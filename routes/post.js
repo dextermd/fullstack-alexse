615d140908ec31558808b4f0d399c8d3663bdb46
dextermd <dextermdpay@gmail.com>
2022-01-05 16:38:25 +0100

    multer

--- routes/post.js
+++ routes/post.js
@@ -2,12 +2,13 @@ const express = require('express');
 const passport = require('passport');
 const upload = require('../middleware/upload');
 const controller = require('../controllers/post');
+const { any } = require('../middleware/upload');
 const router = express.Router();
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
 router.get('/category/:categoryId', controller.getByCategoryId);
-router.post('/', passport.authenticate('jwt', {session: false}), upload.limits(200 * 1024 * 1024).single('image'), controller.create);
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create);
 router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.update);
 router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
 
