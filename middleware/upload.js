63ebf947ca2abb4b659618bd938ef2fede62456f
dextermd <dextermdpay@gmail.com>
2022-01-05 16:57:53 +0100

    multer

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,17 +1,15 @@
-import multer from 'multer';
+const multer = require('multer');
 const moment = require('moment');
 
 const storage = multer.diskStorage({
-    destination(req, file, cb){
-        cb(null, 'uploads/')
-
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
     },
-    filename(req,  file, cb ) {
-        const date = moment().format('DDMMYYYY-HHmmss_SSS')
-        cb(null, `${date}-${file.originalname}`);
-
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "--" + file.originalname);
     }
-});
+});  
+
 
 
 const limits = {
@@ -20,4 +18,7 @@ const limits = {
 
 
 
-module.exports = multer({storage: storage,  limits,});
+
+let upload = multer({ storage: storage, limits, fileFilter: fileFilter,});
+
+export default upload.single('index')
