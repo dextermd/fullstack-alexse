920092f0f56125e782ca6e6143c822cce151f42f
Ruslan Dvornicov <rdvornicov@felicia.md>
2022-07-05 13:11:16 +0300

    post image upload

--- client/src/app/admin/create-post-page/create-post-page.component.ts
+++ client/src/app/admin/create-post-page/create-post-page.component.ts
@@ -35,6 +35,49 @@ export class CreatePostPageComponent implements OnInit {
 
   }
 
+  public options: Object = {
+    charCounterCount: true,
+    // Set the image upload parameter.
+    imageUploadParam: 'image_param',
+
+    // Set the image upload URL.
+    imageUploadURL: 'assets/upload_image',
+
+    // Additional upload params.
+    imageUploadParams: {id: 'my_editor'},
+
+    // Set request type.
+    imageUploadMethod: 'POST',
+
+    // Set max image size to 5MB.
+    imageMaxSize: 5 * 1024 * 1024,
+
+    // Allow to upload PNG and JPG.
+    imageAllowedTypes: ['jpeg', 'jpg', 'png'],
+    events:  {
+  'froalaEditor.initialized':  function () {
+    console.log('initialized');
+  },
+  'froalaEditor.image.beforeUpload':  function  (e,  editor,  images) {
+    //Your code 
+    if  (images.length) {
+      // Create a File Reader.
+      const  reader  =  new  FileReader();
+      // Set the reader to insert images when they are loaded.
+      reader.onload  =  (ev)  =>  {
+        const  result  =  ev.target['result'];
+        editor.image.insert(result,  null,  null,  editor.image.get());
+        console.log(ev,  editor.image,  ev.target['result'])
+      };
+      // Read image as base64.
+      reader.readAsDataURL(images[0]);
+    }
+    // Stop default upload chain.
+    return  false;
+  }
+
+}}
+
   ngOnInit(): void {
     this.postcategoryService.getAllCategory().subscribe(categories => {
       this.categories = categories;
